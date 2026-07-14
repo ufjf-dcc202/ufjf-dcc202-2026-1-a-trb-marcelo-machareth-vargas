2c1540e0f7f00f9b6bbcc16db4180b734b76038a
Marcelo Vargas <marcelo.vargas@estudante.ufjf.br>
2026-07-14 15:58:45 -0300

    adiciona funcao para selecionar um disco

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const pinos = document.querySelectorAll(".pino");
+let discoSelecionado = null;
 
 function adicionaDisco(pino, tam){
     let disco = document.createElement("div");
@@ -6,6 +7,18 @@ function adicionaDisco(pino, tam){
     pino.appendChild(disco);
 }
 
+function selecionaDisco(pino){
+    if(discoSelecionado === null){
+        discoSelecionado = pino.querySelector(":last-child");
+        pino.removeChild(pino.lastChild);
+        console.log("O disco selecionado atualmente é " + discoSelecionado.textContent);
+    }
+}
+
 for(let i = 1; i <= 8; i++){
     adicionaDisco(pinos[0], i);
 }
+
+pinos[0].addEventListener("click", () => selecionaDisco(pinos[0]));
+pinos[1].addEventListener("click", () => selecionaDisco(pinos[1]));
+pinos[2].addEventListener("click", () => selecionaDisco(pinos[2]));
